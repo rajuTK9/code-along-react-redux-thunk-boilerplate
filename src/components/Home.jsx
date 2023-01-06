e388281568ba34c4b4a2f74211081daf3a59b91c
TukadiyaRaju <raju.tukadiya@gmail.com>
2023-01-06 12:38:29 +0530

    new

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <div>
+      <Link to="/list">
+        <button>Fetch Data</button>
+      </Link>
+    </div>
+  );
+}
